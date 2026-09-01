59c159628b3ba536fe48fa73492c8bf8b14b37f6
Natalia Soazo <80857785+NataliaSoazo@users.noreply.github.com>
2026-09-01 20:02:17 -0300

    Validaciones
    Corrección de validaciones, propietarios, inquilinos

--- Models/Inquilino.cs
+++ Models/Inquilino.cs
@@ -9,32 +9,38 @@ public class Inquilino
     [Required(ErrorMessage = "Campo obligatorio")]
     public int Id { get; set; }
     [Display(Name = "NOMBRE")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Ingrese un nombre  válido")]
     [Required(ErrorMessage = "Campo obligatorio")]
     public string? Nombre { get; set; }
     [Display(Name = "APELLIDO")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Ingrese un apellido válido")]
     [Required(ErrorMessage = "Campo obligatorio")]
     public string? Apellido { get; set; }
     [Required(ErrorMessage = "Campo obligatorio")]
     [Display(Name = "E-MAIL")]
+    [StringLength(50, MinimumLength = 13, ErrorMessage = "Ingrese un mail válido")]
     public string? Email { get; set; }
     [Required(ErrorMessage = "Campo obligatorio")]
+    [StringLength(50, MinimumLength = 7, ErrorMessage = "Ingrese un DNI válido")]
     [Display(Name = "DNI")]
     public string? Dni { get; set; }
     [Display(Name = "TELEFONO")]
     [Required(ErrorMessage = "Campo obligatorio")]
+    [StringLength(50, MinimumLength = 10, ErrorMessage = "Ingrese un teléfono válido")]
     public string? Telefono { get; set; }
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Ingrese un domicilio válido")]
     [Display(Name = "DOMICILIO")]
     [Required(ErrorMessage = "Campo obligatorio")]
     public string? Domicilio { get; set; }
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Ingrese una ciudad  válida")]
     [Display(Name = "CIUDAD")]
     [Required(ErrorMessage = "Campo obligatorio")]
     public string? Ciudad { get; set; }
     public string? RequestId { get; set; }
-    // public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
     
-     public override string ToString()
-{
+      public override string ToString()
+    {
     return this.Id + " " + this.Apellido + " " + this.Nombre;
-}
+    }
 }
